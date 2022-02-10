61c806e9ca2732229d3ce7575c7fb97b28ba3e23
Mohak Sharma <mohak327@gmail.com>
2022-02-10 19:17:53 +0530

    revolution component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import Header from "./Sections/Header";
+import Header from './Sections/Header'
+import Revo from './Sections/Revolution'
 
 function App() {
-
   return (
-  <div className="h-full">
-    <Header/>
-    {/* <p>hi</p> */}
+    <div className='h-full bg-dark-900'>
+        <Header />
+        <Revo />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -7,7 +7,7 @@ const Hero = () => {
     <main className='mt-12 mx-auto lg:max-w-8xl px-4 xs:px-12 sm:mt-16 sm:px-6 md:mt-16 lg:mt-16 lg:px-8'>
       <div className='sm:text-center'>
         <h1 className='tracking-tight font-extrabold text-4xl xxxs:text-{5xl} xxs:text-{7xl}  sm:text-6xl lg:text-7xl block text-transparent bg-clip-text bg-gradient-to-r from-violet-500 via-magenta-500 to-bittersweet-500 xl:inline leading-normal'>
-            Design Development Days <br/> 2022 is here!
+          Design Development Days <br /> 2022 is here!
         </h1>
         <p className='xxs:text-3xl lg:text-7xl sm:items-center sm:flex justify-center font-montserrat mt-3 text-base text-white sm:mt-5 text-xl sm:max-w-xl sm:mx-auto md:mt-6 '>
           20th - 23rd March
