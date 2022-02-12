0b82bd3cfa0e04921f343b1d17ef2e5dffdfe7c0
JanitChawla <janitchawla31@gmail.com>
2022-02-12 16:57:16 +0530

    Agenda Section Done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import Expert from "./Sections/Expert";
 import Header from "./Sections/Header";
 import Revo from "./Sections/Revolution";
-import CustomCursor from './components/CustomCursor/index';
+import CustomCursor from "./components/CustomCursor/index";
+import Agenda from "./Sections/Agenda";
 
 function App() {
   return (
     <div className="h-full bg-dark-900">
-    <CustomCursor/>
+      <CustomCursor />
       <Header />
       <Revo />
       <Expert />
+      <Agenda />
     </div>
   );
 }
