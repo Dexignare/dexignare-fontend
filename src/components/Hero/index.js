ac1125b027cec390bf936e90be52a82cb7e89dc9
Mohak Sharma <mohak327@gmail.com>
2022-02-16 10:22:31 +0530

    devfolio button

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 // import ModalButton from '../../components/ModalButton'
 import ScrollButton from './../ScrollButton'
-import Button from './../Button/index'
+// import Button from './../Button/index'
 // import { ReactComponent as TitleSponsors } from '../../assets/titlesponsors.svg'
 
 const Hero = () => {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://apply.devfolio.co/v2/sdk.js'
+    script.async = true
+    script.defer = true
+    document.body.appendChild(script)
+    return () => {
+      document.body.removeChild(script)
+    }
+  }, [])
 
   return (
     <main className='mt-12 mx-auto lg:max-w-8xl px-4 xs:px-12 sm:mt-16 sm:px-6 md:mt-16 lg:mt-16 lg:px-8'>
@@ -15,18 +25,21 @@ const Hero = () => {
         <p className='xxs:text-3xl lg:text-7xl sm:items-center sm:flex justify-center font-montserrat mt-3 text-base text-white sm:mt-5 text-xl sm:max-w-xl sm:mx-auto md:mt-6 '>
           20th - 23rd March
         </p>
-        {/* <TitleSponsors/> */}
-        <img src='../../assets/title.png' alt='' />
-        <div className='absolute left-4 right-4 bottom-32 xxs:bottom-28 xs:bottom-36 xxs:my-4'>
-          <Button text='Register Now' link="https://ddd22.devfolio.co" />
-          <div
-            class='apply-button'
-            data-hackathon-slug='ddd22.devfolio.co'
-            databutton-theme='dark-inverted'
-            style={{ height: '64px', width: '132px' }}
-          ></div>
-        </div>
+
+        {/* absolute left-4 right-4 bottom-32 xxs:bottom-28 xs:bottom-36 xxs:my-4 */}
       </div>
+
+      <div className='flex justify-center my-48 xs:my-24 sm:my-16 md:my-0'>
+        <div
+          className='apply-button'
+          target='_blank'
+          rel='noopener noreferrer'
+          data-hackathon-slug='ddd22.devfolio.co'
+          data-button-theme='dark-inverted'
+          // style={{ height: '84px', width: '312px' }}
+        ></div>
+      </div>
+
       <p class='text-white font-medium uppercase font-montserrat tracking-tight hover:tracking-widest text-center absolute bottom-16 right-0 left-0 px-4 transition ease-in-out delay-250 duration-300'>
         Turn Imagination into Creativity
       </p>
